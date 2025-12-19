6b80d31e36a8b0be5a394c4f562feb017d67b020
Ferenc Pintér <pinterfer@gmail.com>
2025-12-19 15:13:41 +0100

    feat(api): expose L2 cache size environment property
    (cherry picked from commit 842beb9824cfb5c122ce73d8c23a2854bad07c45)
    (cherry picked from commit f94f224fc0a59d45eee04d4231589dbf59e6f005)

--- avs_core/core/avisynth.cpp
+++ avs_core/core/avisynth.cpp
@@ -1146,6 +1146,7 @@ private:
   CacheMode cacheMode;
 
   int cpuFlags;
+  size_t cache_size_L2;
 
   void InitMT();
 };
@@ -2535,6 +2536,7 @@ ScriptEnvironment::ScriptEnvironment()
 #endif
 
   cpuFlags = ::GetCPUFlags();
+  cache_size_L2 = ::GetL2CacheSize();
 
   try {
 #ifdef AVS_WINDOWS
@@ -3212,6 +3214,8 @@ size_t  ScriptEnvironment::GetEnvProperty(AvsEnvProperty prop)
     return nMaxFilterInstances;
   case AEP_PHYSICAL_CPUS:
     return GetNumPhysicalCPUs();
+  case AEP_CACHESIZE_L2:
+    return cache_size_L2;
   case AEP_LOGICAL_CPUS:
     return std::thread::hardware_concurrency();
   case AEP_THREAD_ID:
