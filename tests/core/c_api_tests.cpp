15a32eca1a03e9849f297a5232732d8271fcbbf5
Xinyue Lu <i@7086.in>
2026-09-07 21:21:20 -0700

    test(api): cover global named locks

--- tests/core/c_api_tests.cpp
+++ tests/core/c_api_tests.cpp
@@ -1,5 +1,5 @@
-#include <gtest/gtest.h>
 #include <avisynth_c.h>
+#include <gtest/gtest.h>
 
 namespace avsut::test {
 namespace {
@@ -10,5 +10,19 @@ TEST(CApiValue, VoidStructIsFullyInitialized) {
   EXPECT_EQ(avs_void.d.integer, 0);
 }
 
-}  // namespace
-}  // namespace avsut::test
+TEST(CApiGlobalLock, AcquiresAndReleasesNamedLock) {
+  AVS_ScriptEnvironment *environment =
+      avs_create_script_environment(AVISYNTH_INTERFACE_VERSION);
+  ASSERT_NE(environment, nullptr);
+
+  EXPECT_EQ(avs_acquire_global_lock(environment, "avs-c-api-test-global-lock"),
+            1);
+  EXPECT_EQ(avs_get_error(environment), nullptr);
+  avs_release_global_lock(environment, "avs-c-api-test-global-lock");
+  EXPECT_EQ(avs_get_error(environment), nullptr);
+
+  avs_delete_script_environment(environment);
+}
+
+} // namespace
+} // namespace avsut::test
